cb7f41fed392cecb6cfa5f24751564c5945a17be
paultanmoy005 <tpgd5@mail.missouri.edu>
2025-01-08 19:32:23 -0600

    initial

---
+++ DecoderLayer.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import MultiHeadAttention as mult
+import PositionWiseFeedForward as pff
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(DecoderLayer, self).__init__()
+        self.self_attn = mult.MultiHeadAttention(d_model, num_heads)
+        self.cross_attn = mult.MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = pff.PositionWiseFeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, enc_output, src_mask, tgt_mask):
+        attn_output = self.self_attn(x, x, x, tgt_mask)
+        x = self.norm1(x + self.dropout(attn_output))
+        attn_output = self.cross_attn(x, enc_output, enc_output, src_mask)
+        x = self.norm2(x + self.dropout(attn_output))
+        ff_output = self.feed_forward(x)
+        x = self.norm3(x + self.dropout(ff_output))
+        return x
